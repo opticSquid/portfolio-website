60862b12ee29f90cbfb31f2d30b778314303291a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-11-04 15:55:49 +0530

    starting from scratch

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, Theme } from "@radix-ui/themes";
-
-export default function Home() {
-  return (
-    <Theme appearance="dark" grayColor="sage" accentColor="green">
-      <main>hello</main>
-    </Theme>
-  );
-}
