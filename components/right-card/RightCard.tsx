6e4578a752cae9b0725e78953eed866000da2b1b
Soumalya Bhattacharya <59192460+opticSquid@users.noreply.github.com>
2023-11-08 15:40:08 +0530

    Adding Left Card and Right card Navbar is complete. Adding Hosting to the firebase (#12)
    * adding the branch dev
    
    * adding the branch right-card
    
    * Side Navigation Ui is ready
    
    * removed radix-icon package
    replaced it with only lucide icons
    
    * added a bit animation in the social icons
    It is done in the left card
    
    * decreased the delay in showing tooltip
    It is done in the right card navButton
    
    * UI improvements in the left Card and Meta Data changes (#2)
    
    * Changed the meta data of the website
    Changed the title and description.
    
    * thinned the icons
    
    * increased the gap between mail button and socials
    
    * Edited navigation buttons (#3)
    
    - Removed certifications
    - Edited Past Works to Work History
    - Added Projects button
    
    * UI improvements and meta data changes (#4)
    
    * Changed the meta data of the website
    Changed the title and description.
    
    * thinned the icons
    
    * increased the gap between mail button and socials
    
    * right aligning job role in left card
    
    * Font Changes for better UI and stopping overflow in y axis for the left-card (#5)
    
    * Changed the meta data of the website
    Changed the title and description.
    
    * thinned the icons
    
    * increased the gap between mail button and socials
    
    * right aligning job role in left card
    
    * stopping the overflow of left card in y axis
    
    * updating the version number (#6)
    
    * moved the right card navbar to its own component. (#10)
    
    Also moved the right card components to its own folder
    
    * removing bun lock (#11)
    
    * adding firebase hosting to project
    
    * adding build step of npm istall
    
    * enforcing nodejs 18
    
    * adding build cache
    
    * removing firebase buils folder
    
    * enabling firebase experiment webfraneworks

---
+++ components/right-card/RightCard.tsx
@@ -0,0 +1,12 @@
+import RightCardNavBar from "./RightCardNavBar";
+
+function RightCard() {
+  return (
+    <div className="w-3/5 overflow-auto mt-2 flex flex-row">
+      <div className="w-full">hello</div>
+      <RightCardNavBar />
+    </div>
+  );
+}
+
+export default RightCard;
